8eb771003f8d1edf8a9ee15f48687f1ef99a5f0e
ajdmarques <48205967+ajdmarques@users.noreply.github.com>
2021-12-29 12:23:57 +0000

    Delete code/R/analysis directory

--- code/R/analysis/08-exports.R
+++
@@ -1,6 +0,0 @@
-source('code/R/functions/session_path.R')
-# restore session
-session::restore.session(session_path("07"))
-
-# save session
-session::save.session("data/final/results.rda", compress = "xz")
